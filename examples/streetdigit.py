4d36c1157615d7ce075b1262312f6a8ca38232b7
hzwangjian1 <hzwangjian1@corp.netease.com>
2017-10-27 20:40:16 +0800

    ADD：街景数字识别

---
+++ examples/streetdigit.py
@@ -0,0 +1,8 @@
+"""
+Created by Alex Wang
+On 2017-10-27
+Street view image digit recognization:https://classroom.udacity.com/courses/ud730
+
+ Reading Digits in Natural Images with Unsupervised Feature Learning,2011
+ Multi-digit Number Recognition from Street View Imagery using Deep Convolutional Neural Networks, 2014
+"""
