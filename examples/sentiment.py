c65c63735089da2ea739f77977fe6f5d9e8832ff
hzwangjian1 <hzwangjian1@corp.netease.com>
2017-10-31 10:50:49 +0800

    ADD：添加情感分析模块

---
+++ examples/sentiment.py
@@ -0,0 +1,9 @@
+"""
+Created by Alex Wang
+On 2017-10-31
+Text sentiment:
+            Oreily:https://www.oreilly.com/learning/perform-sentiment-analysis-with-lstms-using-tensorflow
+
+            https://charlesashby.github.io/2017/06/05/sentiment-analysis-with-char-lstm/
+            code:https://github.com/charlesashby/CharLSTM
+"""
