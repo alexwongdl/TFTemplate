9e4e9e1596c93091c0ed9e5435dbe3c0c1db579c
Alex.W <wangjian90@hotmail.com>
2017-11-05 13:26:11 +0800

    MOD：添加ptb启动脚本

---
+++ bash_scripts/start_ptb_train.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+python MainEntryMnist.py \
+    --task=train_mnist \
+    --is_training=True \
+    --max_iter=200000 \
+    --batch_size=16 \
+    --learning_rate=0.01 \
+    --decay_step=10000 \
+    --decay_rate=0.9 \
+    --input_dir=/home/recsys/hzwangjian1/learntf/MNIST \
+    --save_model_dir=/home/recsys/hzwangjian1/learntf/mnist_model \
+    --save_model_freq=10000 \
+    --summary_dir=/home/recsys/hzwangjian1/learntf/mnist_summary
