be35397659333bef8a640daf0286c6fa3f05f9e1
Alex.W <wangjian90@hotmail.com>
2017-12-04 22:15:01 +0800

    MOD：ctpn

--- examples/testdetector.py
+++ examples/testdetector.py
@@ -2,6 +2,8 @@
 Created by Alex Wang
 On 2017-10-27
 
+CTPN：detecting text in natural image with connectionist text proposal network
+    https://github.com/eragonruan/text-detection-ctpn
 EAST:An Efficient and Accurate Scene Text Detector, 2017
 """
 
