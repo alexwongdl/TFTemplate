c7300d3d4083257474264d6410f1c07ae67ba036
hzwangjian1 <hzwangjian1@corp.netease.com>
2017-12-05 20:06:05 +0800

    MOD：mini mod

--- examples/testdetector.py
+++ examples/testdetector.py
@@ -2,10 +2,8 @@
 Created by Alex Wang
 On 2017-10-27
 
+Fast-RCNN:https://github.com/CharlesShang/TFFRCNN
 CTPN：detecting text in natural image with connectionist text proposal network
     https://github.com/eragonruan/text-detection-ctpn
 EAST:An Efficient and Accurate Scene Text Detector, 2017
 """
-
-from tensorflow.contrib import slim
-slim.conv2d()
