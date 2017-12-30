1c696124c58f200e1b21f9d15f6c4a4806401f01
Alex.W <wangjian90@hotmail.com>
2017-12-30 15:18:02 +0800

    ADD：faster-rcnn 添加object类别预测和验证集验证模型效果

--- examples/testdetector.py
+++ examples/testdetector.py
@@ -23,7 +23,7 @@ from examples.objectdetect.class_info import voc_classes, voc_classes_num
 
 
 def faster_rcnn_model(x_input, reuse, is_training, FLAGS, anchor_set_size=9, fea_map_inds=None, box_reg=None, cls=None,
-                      object_cls=None, cal_loss = True):
+                      object_cls=None, cal_loss=True):
     """
     :param x_input: [batch, None, None, 3]
     :param reuse:
@@ -114,15 +114,18 @@ def faster_rcnn_model(x_input, reuse, is_training, FLAGS, anchor_set_size=9, fea
             pred_box = tl.layers.Conv2d(network, n_filter=4 * anchor_set_size, filter_size=(1, 1), strides=(1, 1),
                                         padding='SAME', name='box_pred')  # [batch_size, W, H , 4k]
 
-        if not cal_loss: ##测试阶段，不需要计算损失函数
-            return pred_cls, pred_box, conv5_3
+            pred_obj_cls = tl.layers.Conv2d(network, n_filter=voc_classes_num * anchor_set_size, filter_size=(1, 1),
+                                            strides=(1, 1), padding='SAME', name='obj_cls_pred')
 
-        #损失函数
-        shape_ind = tf.shape(pred_cls.outputs)  #[image_ind, fea_map_w, fea_map_h, anchors * 2]
+        if not cal_loss:  ##测试阶段，不需要计算损失函数
+            return pred_cls, pred_box, pred_obj_cls, conv5_3
+
+        # 损失函数
+        shape_ind = tf.shape(pred_cls.outputs)  # [image_ind, fea_map_w, fea_map_h, anchors * 2]
         shape_ind = tf.expand_dims(shape_ind, 0)
-        shape_ind = tf.slice(shape_ind, [0, 0], [1, 3]) #[image_ind, fea_map_w, fea_map_h]
+        shape_ind = tf.slice(shape_ind, [0, 0], [1, 3])  # [image_ind, fea_map_w, fea_map_h]
 
-        # cls reshape and calculate loss
+        # 1. cls reshape and calculate loss
         shape_cls = tf.concat([shape_ind, tf.convert_to_tensor([[anchor_set_size, 2]])], 1)
         shape_cls = tf.squeeze(shape_cls)
         pred_cls_reshape = tf.reshape(pred_cls.outputs, shape_cls)
@@ -130,9 +133,9 @@ def faster_rcnn_model(x_input, reuse, is_training, FLAGS, anchor_set_size=9, fea
 
         box_label = tf.argmax(cls, axis=1)
         loss_cls = tl.cost.cross_entropy(pred_cls_use, box_label, name='class_cost_entropy')
-        loss_cls =  tf.reduce_sum(loss_cls) / FLAGS.batch_size  # about 0.01
+        loss_cls = tf.reduce_sum(loss_cls) / FLAGS.batch_size  # about 0.01
 
-        # box_reg reshape and cllculate loss
+        # 2. box_reg reshape and calculate loss
         shape_box = tf.concat([shape_ind, tf.convert_to_tensor([[anchor_set_size, 4]])], 1)
         shape_box = tf.squeeze(shape_box)
         pred_box_reshape = tf.reshape(pred_box.outputs, shape_box)
@@ -143,19 +146,30 @@ def faster_rcnn_model(x_input, reuse, is_training, FLAGS, anchor_set_size=9, fea
         y1 = 0.5 * box_diff_abs ** 2
         y2 = box_diff_abs - 0.5
         loss_box = tf.where(tf.less(box_diff_abs, tf.ones_like(box_diff_abs)), y1, y2)
-        loss_box = tf.reduce_sum(loss_box) / FLAGS.batch_size  # about
+        loss_box = tf.reduce_sum(loss_box) / FLAGS.batch_size  # about 3
+
+        # 3. object classfication and calculate loss
+        shape_obj_cls = tf.concat([shape_ind, tf.convert_to_tensor([[anchor_set_size, voc_classes_num]])], 1)
+        shape_obj_cls = tf.squeeze(shape_obj_cls)
+        pred_obj_cls_reshape = tf.reshape(pred_obj_cls.outputs, shape_obj_cls)
+        pred_obj_cls_used = tf.gather_nd(pred_obj_cls_reshape, fea_map_inds)
+
+        obj_cls_label = tf.argmax(object_cls, axis=1)
+        loss_obj_cls = tl.cost.cross_entropy(pred_obj_cls_used, obj_cls_label, name='class_cost_entropy')
+        loss_obj_cls = tf.reduce_sum(loss_obj_cls) / FLAGS.batch_size  # about
 
         # TODO: loss 添加 box reg loss和类别预测loss
-        loss = loss_box
+        loss = 300 * loss_cls + loss_box + 100 * loss_obj_cls
         cost = loss
 
-        return pred_cls, pred_box, loss, cost, conv5_3
+        return pred_cls, pred_box, pred_obj_cls, loss, cost, loss_cls, loss_box, loss_obj_cls, conv5_3
+
 
 def process_one_image(data):
     result = {}
     index = data['index']
     image = Image.open(data['image_path'])
-    result['image']= np.array(image)
+    result['image'] = np.array(image)
 
     fea_map_inds_batch = []
     box_reg_batch = []
@@ -179,6 +193,7 @@ def process_one_image(data):
     result['box_class'] = box_class_batch
     result['object_class'] = object_class_batch
     return result
+
 
 def batch_preprocess(train_data_batch):
     """
@@ -209,7 +224,8 @@ def batch_preprocess(train_data_batch):
         box_class_batch.extend(data['box_class'])
         object_class_batch.extend(data['object_class'])
 
-    return x_train_batch, fea_map_inds_batch,box_reg_batch, box_class_batch,object_class_batch, fea_map_shape_batch
+    return x_train_batch, fea_map_inds_batch, box_reg_batch, box_class_batch, object_class_batch, fea_map_shape_batch
+
 
 def train_faster_rcnn(FLAGS):
     print("start train faster rcnn model")
@@ -232,22 +248,22 @@ def train_faster_rcnn(FLAGS):
     # 3.build graph：including loss function，learning rate decay，optimization operation
     print('start build graph...')
     x_train = tf.placeholder(tf.float32, shape=[FLAGS.batch_size, None, None, 3], name='x_train')
-    fea_map_inds = tf.placeholder(tf.int32, shape=[None, 4], name='fea_map_inds')  #[Image_ind, W_ind, H_ind, k_ind]
+    fea_map_inds = tf.placeholder(tf.int32, shape=[None, 4], name='fea_map_inds')  # [Image_ind, W_ind, H_ind, k_ind]
     box_reg = tf.placeholder(tf.float32, shape=[None, 4], name='box_reg')
     box_class = tf.placeholder(tf.int16, shape=[None, 2], name='box_class')
     object_class = tf.placeholder(tf.int16, shape=[None, voc_classes_num], name='object_class')
 
-    #def faster_rcnn_model(x_input, reuse, is_training, FLAGS, anchor_set_size=9,
+    # def faster_rcnn_model(x_input, reuse, is_training, FLAGS, anchor_set_size=9,
     #                       fea_map_inds=None, box_reg=None, cls=None, object_cls=None, cal_loss = True):
     # pred_cls_train, pred_box_train, loss_train, cost_train = faster_rcnn_model(
     #         x_train_batch, reuse=False, is_training=True, FLAGS=FLAGS, fea_map_inds=fea_map_inds_batch,
     #         box_reg=box_reg_batch, cls=box_class_batch, object_cls=object_class_batch, cal_loss=True)  # train
-    pred_cls_train, pred_box_train, loss_train, cost_train, conv5_3 = faster_rcnn_model(
-            x_input = x_train, reuse=False, is_training=True, FLAGS=FLAGS, fea_map_inds=fea_map_inds,
+    pred_cls_train, pred_box_train, pred_obj_cls_train, loss_train, cost_train, loss_cls_train, loss_box_train, loss_obj_cls_train, conv5_3 = faster_rcnn_model(
+            x_input=x_train, reuse=False, is_training=True, FLAGS=FLAGS, fea_map_inds=fea_map_inds,
             box_reg=box_reg, cls=box_class, object_cls=object_class, cal_loss=True)  # train
 
-    pred_cls_pred, pred_box_pred, loss_pred, cost_pred, _ = faster_rcnn_model(
-            x_input = x_train, reuse=True, is_training=False, FLAGS=FLAGS, fea_map_inds=fea_map_inds,
+    pred_cls_pred, pred_box_pred, pred_obj_cls_pred, loss_pred, cost_pred, loss_cls_pred, loss_box_pred, loss_obj_cls_pred, _ = faster_rcnn_model(
+            x_input=x_train, reuse=True, is_training=False, FLAGS=FLAGS, fea_map_inds=fea_map_inds,
             box_reg=box_reg, cls=box_class, object_cls=object_class, cal_loss=True)  # train)  # validate/test
 
     global_step = tf.contrib.framework.get_or_create_global_step()
@@ -257,16 +273,18 @@ def train_faster_rcnn(FLAGS):
     #                                           end_learning_rate=0.00001)
     incr_global_step = tf.assign(global_step, global_step + 1)
 
-    #TODO:修改需要训练的模型参数，冻结VGG16变量
+    # TODO:修改需要训练的模型参数，冻结VGG16变量
     # train_params = net.all_params
     train_params = []
     pred_cls_params = pred_cls_train.all_params
     pred_box_params = pred_box_train.all_params
-    # train_params.append(pred_cls_params[20:])
+    pred_obj_cls_params = pred_obj_cls_train.all_params
+    train_params.append(pred_cls_params[20:])
     train_params.append(pred_box_params[20:])
+    train_params.append(pred_obj_cls_params[20:])
     # train_params = tf.trainable_variables()
-    train_op = tf.train.AdamOptimizer(learning_rate, beta1=0.9, beta2=0.999, epsilon=1e-08, use_locking=False).minimize(
-            cost_train, var_list=train_params)
+    train_op = tf.train.AdamOptimizer(learning_rate, beta1=0.9, beta2=0.999, epsilon=1e-08, use_locking=False).\
+                        minimize(cost_train, var_list=train_params)
 
     # train_vars = tf.trainable_variables()
     # grads, _ = tf.clip_by_global_norm(tf.gradients(cost_train, train_vars),
@@ -294,7 +312,7 @@ def train_faster_rcnn(FLAGS):
             # 加载VGG16预训练模型
             npz = np.load(FLAGS.vgg16_path)
             vgg16_params = []
-            for val in sorted( npz.items() ):
+            for val in sorted(npz.items()):
                 print("  Loading %s" % str(val[1].shape))
                 vgg16_params.append(val[1])
             tl.files.assign_params(sess, vgg16_params[0:26], conv5_3)
@@ -311,15 +329,15 @@ def train_faster_rcnn(FLAGS):
 
         for step in range(FLAGS.max_iter):
             permutation_ind = np.random.permutation(len(train_data_set))
-            x_train_batch, fea_map_inds_batch, box_reg_batch, box_class_batch, object_class_batch, fea_map_shape_batch =\
+            x_train_batch, fea_map_inds_batch, box_reg_batch, box_class_batch, object_class_batch, fea_map_shape_batch = \
                 batch_preprocess(train_data_set[permutation_ind[:FLAGS.batch_size]])
             # x_train = tf.placeholder(tf.float32, [FLAGS.batch_size, None, None, 3])
             # fea_map_inds = tf.placeholder(tf.int16, [None, 4])  #[Image_ind, W_ind, H_ind, k_ind]
             # box_reg = tf.placeholder(tf.float32, [None, 4])
             # box_class = tf.placeholder(tf.int16, [None, 2])
             # object_class = tf.placeholder(tf.int16, [None, voc_classes_num])
-            feed_dict = {x_train:x_train_batch, fea_map_inds:fea_map_inds_batch, box_reg:box_reg_batch,
-                         box_class:box_class_batch, object_class:object_class_batch}
+            feed_dict = {x_train: x_train_batch, fea_map_inds: fea_map_inds_batch, box_reg: box_reg_batch,
+                         box_class: box_class_batch, object_class: object_class_batch}
             start_time = time.time()
             fetches = {'train_op': train_op, 'global_step': global_step, 'inc_global_step': incr_global_step}
 
@@ -327,23 +345,27 @@ def train_faster_rcnn(FLAGS):
                 fetches['cost'] = cost_train
                 fetches['learning_rate'] = learning_rate
                 fetches['pred_cls_train'] = pred_cls_train.outputs
+                fetches['loss_cls_train'] = loss_cls_train
+                fetches['loss_box_train'] = loss_box_train
+                fetches['loss_obj_cls_train'] = loss_obj_cls_train
 
             if (step + 1) % FLAGS.summary_freq == 0:
                 # fetches['summary_op'] = sv.summary_op  # sv.summary_op = summary.merge_all()
                 fetches['summary_op'] = tf.summary.merge_all()
 
-            result = sess.run(fetches, feed_dict = feed_dict)
+            result = sess.run(fetches, feed_dict=feed_dict)
 
             if (step + 1) % FLAGS.summary_freq == 0:
                 # sv.summary_computed(sess, result['summary_op'], global_step=result['global_step'])
-                summary_writer.add_summary(result['summary_op'],result['global_step'])
-
+                summary_writer.add_summary(result['summary_op'], result['global_step'])
 
             if (step + 1) % FLAGS.print_info_freq == 0:
                 rate = FLAGS.batch_size / (time.time() - start_time)
                 print("step:{}\t, rate:{:.2f} images/sec".format(step + 1, rate))
                 print("global step:{}".format(result['global_step']))
                 print("cost:{:.4f}".format(result['cost']))
+                print("loss_cls_train:{:.4f}, loss_box_train:{:.4f}, loss_obj_cls_train:{:.4f}".
+                      format(result['loss_cls_train'], result['loss_box_train'], result['loss_obj_cls_train']))
                 print("learning rate:{:.6f}".format(result['learning_rate']))
                 # pred_value = result['pred_cls_train']
                 # print(pred_value[0:50])
@@ -355,40 +377,24 @@ def train_faster_rcnn(FLAGS):
                 saver.save(sess, os.path.join(FLAGS.save_model_dir, 'model'), global_step=global_step)
 
             if (result['global_step'] + 1) % FLAGS.valid_freq == 0 or step == 0:
-                print("validate model...")  #TODO
-                # permutation_ind = np.random.permutation(len(train_data_set))
-                # x_train_batch, fea_map_inds_batch, box_reg_batch, box_class_batch, object_class_batch, fea_map_shape_batch = \
-                #         batch_preprocess(train_data_set[permutation_ind[:FLAGS.batch_size]])
-                # feed_dict = {x_train:x_train_batch, fea_map_inds:fea_map_inds_batch, box_reg:box_reg_batch,
-                #              box_class:box_class_batch, object_class:object_class_batch}
-                # fetches['cost'] = cost_train
-                # fetches['learning_rate'] = learning_rate
-                # fetches['pred_cls_train'] = pred_cls_train.outputs
-                # result = sess.run(fetches, feed_dict = feed_dict)
-                # pred_value = result['pred_cls_train']
-                # print(pred_value[0:50])
-                #----------------------------------------------------------------------------------
-                # valid_cost = 0.0
-                # fetches = {'cost_valid': cost_valid, 'predict_valid': predict}
-                # # net_valid, cost_valid, encode_net_valid, encode_net_valid, _, predict
-                # valid_num = 100
-                # for valid_index in range(valid_num):
-                #     valid_data = valid_data_list[valid_index]
-                #     feed_dict = {
-                #         x_train: valid_data['x_list'], y_train: valid_data['y_list'],
-                #         target: valid_data['target_list'], x_length: valid_data['x_len_list'],
-                #         y_length: valid_data['y_len_list'], target_weight: valid_data['target_weight']
-                #     }
-                #     result_valid = sess.run(fetches, feed_dict=feed_dict)
-                #     valid_cost += result_valid['cost_valid']
-                # valid_cost /= valid_num
-                # # print("average valid cost:{:.5f} on {} sentences".format(valid_cost, valid_num * FLAGS.batch_size))
-                # printutil.mod_print("average valid cost:{:.5f} on {} sentences".format(valid_cost,
-                #                                                                        valid_num * FLAGS.batch_size),
-                #                     fg=printutil.ANSI_WHITE, bg=printutil.ANSI_GREEN_BACKGROUND,
-                #                     mod=printutil.MOD_UNDERLINE)
+                print("validate model...")
+                permutation_ind = np.random.permutation(len(valid_data_set))
+                x_train_batch, fea_map_inds_batch, box_reg_batch, box_class_batch, object_class_batch, fea_map_shape_batch = \
+                        batch_preprocess(train_data_set[permutation_ind[:FLAGS.batch_size]])
+                feed_dict = {x_train:x_train_batch, fea_map_inds:fea_map_inds_batch, box_reg:box_reg_batch,
+                             box_class:box_class_batch, object_class:object_class_batch}
+                fetches['cost_pred'] = cost_pred
+                fetches['loss_cls_pred'] = loss_cls_pred
+                fetches['loss_box_pred'] = loss_box_pred
+                fetches['loss_obj_cls_pred'] = loss_obj_cls_pred
+                result = sess.run(fetches, feed_dict = feed_dict)
+                print("cost_pred:{:.4f}".format(result['cost_pred']))
+                print("loss_cls_pred:{:.4f}, loss_box_pred:{:.4f}, loss_obj_cls_pred:{:.4f}".
+                      format(result['loss_cls_pred'], result['loss_box_pred'], result['loss_obj_cls_pred']))
+
         print('optimization finished!')
         summary_writer.close()
+
 
 # 6.testing
 def test(FLAGS):
